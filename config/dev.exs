f421b20b78c12bac0dfccd863aac595171aa0d82
tarrynn <catalin.gheonea@gmail.com>
2019-10-07 11:10:54 +0100

    dev db

--- config/dev.exs
+++ config/dev.exs
@@ -3,9 +3,9 @@ use Mix.Config
 # Configure your database
 config :lixi, Lixi.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "lixi_dev",
-  hostname: "localhost",
+  hostname: "docker",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
 
