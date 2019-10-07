c71085a87b81e5ed7f71553a89068fb93de29feb
tarrynn <catalin.gheonea@gmail.com>
2019-10-07 11:36:14 +0100

    static creds for prod for this test

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -4,24 +4,14 @@
 # remember to add this file to your .gitignore.
 use Mix.Config
 
-database_url =
-  System.get_env("DATABASE_URL") ||
-    raise """
-    environment variable DATABASE_URL is missing.
-    For example: ecto://USER:PASS@HOST/DATABASE
-    """
+database_url = "ecto://postgres:@docker/lixi_dev"
 
 config :lixi, Lixi.Repo,
   # ssl: true,
   url: database_url,
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
 
-secret_key_base =
-  System.get_env("SECRET_KEY_BASE") ||
-    raise """
-    environment variable SECRET_KEY_BASE is missing.
-    You can generate one by calling: mix phx.gen.secret
-    """
+secret_key_base = "aAXB7qcME4RKCYGbwCkuiV3YT2C6vv5Aerf4OUjcN1oH3nCjuLBVFPa8SMDa2dvH"
 
 config :lixi, LixiWeb.Endpoint,
   http: [:inet6, port: String.to_integer(System.get_env("PORT") || "4000")],
@@ -32,7 +22,7 @@ config :lixi, LixiWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-#     config :lixi, LixiWeb.Endpoint, server: true
+config :lixi, LixiWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
