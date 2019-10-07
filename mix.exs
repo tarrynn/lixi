f589dfc3ead6792618866baf86422f0602a3b6cc
tarrynn <catalin.gheonea@gmail.com>
2019-10-07 17:06:57 +0100

    new release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Lixi.MixProject do
   def project do
     [
       app: :lixi,
-      version: "0.1.7",
+      version: "0.1.8",
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
