658d6e50048a2a46bcb9555d9d422a86627ecc72
tarrynn <catalin.gheonea@gmail.com>
2019-10-07 13:21:47 +0100

    bump to v0.1.4

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -22,7 +22,7 @@ config :lixi, LixiWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-config :lixi, LixiWeb.Endpoint, server: true
+# config :lixi, LixiWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.
