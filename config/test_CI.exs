c991b0607586c53a94a8a9063b9bbff5d0b87926
tarrynn <catalin.gheonea@gmail.com>
2019-10-16 09:43:38 +0100

    fixes to psql container service

--- config/test_CI.exs
+++ config/test_CI.exs
@@ -5,7 +5,7 @@ config :lixi, Lixi.Repo,
   username: "postgres",
   password: "",
   database: "lixi_test",
-  hostname: "docker",
+  hostname: "postgres",
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
