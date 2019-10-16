7dad121a8d63a12969004eb526c90cae5261e42b
tarrynn <catalin.gheonea@gmail.com>
2019-10-16 09:40:18 +0100

    using postgres in parallel container

--- config/test_CI.exs
+++ config/test_CI.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :lixi, Lixi.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "lixi_test",
   hostname: "docker",
   pool: Ecto.Adapters.SQL.Sandbox
